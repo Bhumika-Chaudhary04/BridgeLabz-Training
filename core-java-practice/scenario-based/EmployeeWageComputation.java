b32e1baee78103540bb5775d2c7c356edcb7f80d
Bhumika Chaudhary <bhumi333333@gmail.com>
2025-12-29 22:14:27 +0530

    [bhumika chaudhary] Add. Implemented UC-2 :Get Employee type in Employee Wage Computation

--- core-java-practice/scenario-based/EmployeeWageComputation.java
+++ core-java-practice/scenario-based/EmployeeWageComputation.java
@@ -1,13 +1,29 @@
 package scenario_based;
+
 import java.util.Random;
+
 public class EmployeeWageComputation {
+    // Constants
+    static final int wagePerHour = 20;
+    static final int fullDayHour = 8;
+    static final int partTimeHour = 4;
+    static final int maxWorkingDays = 20;
+    static final int maxWorkingHours = 100;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch\n");
     }
+
     // Use Case 1: Check if employee is present or absent
     public static boolean isEmployeePresent() {
         Random random = new Random();
         return random.nextInt(2) == 1;
     }
-    
+
+    // UC2: Get employee type (1 = Part Time, 2 = Full Time)
+    public static int getEmployeeType() {
+        Random random = new Random();
+        return random.nextInt(2) + 1;
+    }
+
 }

9181ef11b475b61991f071b7068e0a381e8b4061
Bhumika Chaudhary <bhumi333333@gmail.com>
2025-12-30 11:33:29 +0530

    [bhumika chaudhary] Refactor. UC2: Checking if length of two lines are equal

--- core-java-practice/scenario-based/LineComparison.java
+++ core-java-practice/scenario-based/LineComparison.java
@@ -2,10 +2,10 @@ package scenario_based;
 import java.util.*;
 public class LineComparison {
 	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Computation Program");
+		System.out.println("Welcome to Line Comparison Computation Program\n");
 		Scanner sc=new Scanner(System.in);
 		//use case 1: Calculate length of a line using co-ordinates of x,y
-		//user input of co-ordiantes of x and y
+		//user input 
 		System.out.print("Enter x1 : ");
 		int x1=sc.nextInt();
 		System.out.print("Enter y1 : ");
@@ -15,7 +15,28 @@ public class LineComparison {
 		System.out.print("Enter y2 : ");
 		int y2=sc.nextInt();
 		//computation
-		double length=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
-		System.out.printf("Length of the line is : %.2f",length);
+		Double lengthOfLineOne=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+		System.out.printf("Length of the line 1 is : %.2f\n",lengthOfLineOne);
+		
+		System.out.print("Enter x3 : ");
+		int x3=sc.nextInt();
+		System.out.print("Enter y3 : ");
+		int y3=sc.nextInt();
+		System.out.print("Enter x4 : ");
+		int x4=sc.nextInt();
+		System.out.print("Enter y4 : ");
+		int y4=sc.nextInt();
+		//computation
+		Double lengthOfLineTwo=Math.sqrt(Math.pow((x4-x3),2)+Math.pow((y4-y3),2));
+		System.out.printf("Length of the line 2 is : %.2f\n",lengthOfLineTwo);
+				
+		//use case 2:compare lengths of two lines
+		if(lengthOfLineOne.equals(lengthOfLineTwo)) {
+			System.out.println("Both lines are of equal length");
+		}
+		else {
+			System.out.println("Lines are not of equal length");
+		}
+		
 	}
 }

0ea11fa2da0c00fbdd83e53296a1feb07d467c25
Bhumika Chaudhary <bhumi333333@gmail.com>
2026-01-07 16:32:20 +0530

    [bhumika chaudhary] Add .Added Cab Booking or Ride Management System and Online Banking System

---
+++ oops-practice/scenario-based/Account.java
@@ -0,0 +1,38 @@
+package OnlineBankingManagementSystem;
+import java.util.*;
+abstract class Account implements BankService {
+    protected int accountNumber;
+    protected String holderName;
+    protected double balance;
+    protected List<String> transactions = new ArrayList<>();
+    public Account(int accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+    public synchronized void deposit(double amount) {
+        balance += amount;
+        transactions.add("Deposited ₹" + amount);
+    }
+    public synchronized void withdraw(double amount) throws InsufficientBalanceException {
+        if (balance < amount)
+            throw new InsufficientBalanceException("❌ Insufficient Balance");
+        balance -= amount;
+        transactions.add("Withdrawn ₹" + amount);
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public synchronized void transfer(Account target, double amount)
+            throws InsufficientBalanceException {
+        this.withdraw(amount);
+        target.deposit(amount);
+        transactions.add("Transferred " + amount + " to A/C " + target.accountNumber);
+        target.transactions.add("Received " + amount + " from A/C " + this.accountNumber);
+    }
+    public void showTransactions() {
+        System.out.println("\n Transaction History:");
+        for (String t : transactions)
+            System.out.println(t);
+    }
+}
---
+++ oops-practice/scenario-based/BankService.java
@@ -0,0 +1,6 @@
+package OnlineBankingManagementSystem;
+interface BankService {
+    void deposit(double amount);
+    void withdraw(double amount) throws InsufficientBalanceException;
+    double calculateInterest();
+}

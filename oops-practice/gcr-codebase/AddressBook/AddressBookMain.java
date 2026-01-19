fefcd766e754eddf9a7ebb1f383068af1610cb0f
Bhumika Chaudhary <bhumi333333@gmail.com>
2026-01-19 21:48:13 +0530

    [bhumika chaudhary] Refactor .Added UC4: Implement deletion of contact using first and last name

--- oops-practice/gcr-codebase/AddressBook/AddressBookMain.java
+++ oops-practice/gcr-codebase/AddressBook/AddressBookMain.java
@@ -1,12 +1,15 @@
 package AddressBook;
+
 import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("------------Welcome to Address Book Program----------------");
         Scanner sc = new Scanner(System.in);
-        //UC2 :using console to take contact details from user and add them to AddressBook
+        // UC2 :using console to take contact details from user and add them to
+        // AddressBook
         AddressBook addressBook = new AddressBook();
-        //console input
+        // console input
         System.out.print("Enter First Name: ");
         String firstName = sc.nextLine();
         System.out.print("Enter Last Name: ");
@@ -23,19 +26,26 @@ public class AddressBookMain {
         String phoneNumber = sc.nextLine();
         System.out.print("Enter Email: ");
         String email = sc.nextLine();
-        //create contact object
-        ContactPerson person = new ContactPerson(firstName,lastName,address,city,state,zip,phoneNumber,email);
-        //add contact to addressBook
+        // create contact object
+        ContactPerson person = new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        // add contact to addressBook
         addressBook.addContact(person);
-        //display contacts
+        // display contacts
         addressBook.displayContacts();
-        //edit contact
+        // edit contact
         System.out.print("\nEnter first name of the contact to edited : ");
-        firstName=sc.nextLine();
+        firstName = sc.nextLine();
         System.out.print("Enter last name of the contact to be edited : ");
-        lastName=sc.nextLine();
-        //UC3:edit contact
+        lastName = sc.nextLine();
+        // UC3:edit contact
         addressBook.editContact(firstName, lastName);
+        addressBook.displayContacts();
+        // UC4:delete contact
+        System.out.println("Enter first name of the contact to be deleted : ");
+        firstName = sc.nextLine();
+        System.out.println("Enter last name of the person to be deleted : ");
+        lastName = sc.nextLine();
+        addressBook.deleteContact(firstName, lastName);
         addressBook.displayContacts();
         sc.close();
     }
